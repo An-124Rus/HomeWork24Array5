aea4ebd0e2cbe6c4b518a90c40042ac206a40b38
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-09-13 16:42:20 +0300

    HomeWork24Array5

---
+++ HomeWork24Array5/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int minValue = 0;
+        int maxValue = 10;
+        int firstNumbersCount = 0;
+        int secondNumbersCount = 0;
+
+        Random randome = new Random();
+
+        Console.Write("Введите длину массива: ");
+        int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[arrayLength];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = randome.Next(minValue, maxValue);
+
+            Console.Write(array[i] + " ");
+        }
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] == array[i - 1])
+            {
+                firstNumbersCount++;
+            }
+            else if (array[i] != array[i - 1] && firstNumbersCount > secondNumbersCount + 1)
+            {
+                secondNumbersCount = firstNumbersCount + 1;
+                firstNumbersCount = 0;
+
+                Console.WriteLine($"\nЧисло {array[i - 1]} повторяется {secondNumbersCount} раз(а).");
+
+                secondNumbersCount = 0;
+            }
+        }
+
+        if (firstNumbersCount == 0 && secondNumbersCount == 0)
+            Console.WriteLine($"\nНет подмассива повторяющихся чисел.");
+    }
+}
